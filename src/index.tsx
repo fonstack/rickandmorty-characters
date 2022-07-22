d6e1f51288c104b38fe2bd0e91a8138f4f97fe72
Carlos Fontes <fonstack@gmail.com>
2022-07-22 20:33:04 +0100

    index react 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import Root from './application/Root';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Root />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
