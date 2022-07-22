2ac4ceb18ff93e5b3c480d6bcf75889530673650
Carlos Fontes <fonstack@gmail.com>
2022-07-22 15:46:23 +0100

    Added links on TopBar

--- src/componets/TopBar/TopBar.tsx
+++ src/componets/TopBar/TopBar.tsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+import { Paths } from '../../navigation';
+
 const TopBar = () => {
-  return <h3>Topbar</h3>;
+  return (
+    <>
+      <h3>Topbar</h3>
+      <Link to={Paths.MARKETPLACE}>MARKETPLACE</Link>
+      <Link to={Paths.FAVORITES}>FAVORITES</Link>
+    </>
+  );
 };
 
 export { TopBar };
