667c602f4aa54d851cda35fe18f181a6691ca454
Carlos Fontes <fonstack@gmail.com>
2022-07-24 05:28:21 +0100

    removed unused import

--- src/pages/MarketplacePage/styles.ts
+++ src/pages/MarketplacePage/styles.ts
@@ -1,5 +1,4 @@
 import styled, { css } from 'styled-components';
-import { Button } from '../../componets/Button/Button';
 import { getFontSize, getSpacing } from '../../styles';
 
 export const StyledTitle = styled.div`
